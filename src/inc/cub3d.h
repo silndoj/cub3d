45d24c88286ab5c7b184c9031bcc289e4ef6b767
tndreka <tndreka@student.42heilbronn.de>
2025-03-18 16:52:07 +0100

    EDIT: added a 2d array for map, Header cleanup

--- src/inc/cub3d.h
+++ src/inc/cub3d.h
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42heilbronn.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 15:14:38 by tndreka           #+#    #+#             */
+/*   Updated: 2025/03/18 16:34:39 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 21:37:21 by tndreka           #+#    #+#             */
@@ -14,11 +26,6 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-/*
-========== INCLUDES =========
-*/
-
-
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -26,32 +33,25 @@
 # include <math.h>
 # include <string.h>
 
-/*
- * ========= STRUCTURES =========
- *
- * */
+#include "get_next_line.h"
+#include "garbage_collector.h"
+
+
 typedef struct s_parser
 {
+    char **map2d;
 }   t_parser;
 
 typedef struct s_game
 {
 }   t_game;
 
-/*
- * ========= PROTOTYPES =========
- *
- * */
-int parse_cub(int argc, char **argv);
-int check_map(char *file);
 
+int parse_cub(int argc, char **argv, t_parser *parser);
+int check_map(char *file, t_parser *parser);
+void    copy_map(int fd, t_parser *parser);
+void print_map(t_parser *parser);
 
-
-
-/*
-    * ========= UTILS =========
-    *
-*/
 int ft_strlen(char *str);
 int ft_strcmp(char *s1, char *s2);
 #endif

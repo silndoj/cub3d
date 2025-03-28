1f689de581c4e23f4c0705e6d9b808d73e9f6bbc
tndreka <tndreka@student.42heilbronn.de>
2025-03-28 20:04:12 +0100

    ADD:added the function for the initialization of the textures

---
+++ src/game_build/init_map.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42heilbronn.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/28 18:05:47 by tndreka           #+#    #+#             */
+/*   Updated: 2025/03/28 18:05:47 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+void    init_textures(t_parser *parser)
+{
+    parser->no_texture = NULL;
+    //parser->so_texture = NULL;
+    //parser->we_texture = NULL;
+    //parser->ea_texture = NULL;
+
+}
+

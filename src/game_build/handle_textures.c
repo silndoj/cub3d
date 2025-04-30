bf8d6e36706d894e74edc3135630e0427e73a271
tndreka <tndreka@student.42heilbronn.de>
2025-04-30 19:43:56 +0200

    EDIT: removed unnecesary lines

--- src/game_build/handle_textures.c
+++ src/game_build/handle_textures.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_textures.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka <tndreka@student.42heilbronn.fr>   +#+  +:+       +#+        */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:23:58 by tndreka           #+#    #+#             */
-/*   Updated: 2025/03/28 19:24:02 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/04/30 19:25:12 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,10 @@ int    parse_textures(t_parser *parser)
     init_textures(parser);
     while(parser->map2d[++i])
     {
-        trim = ft_strtrim(parser->map2d[i], " \t");
-        if(!trim || !*trim)
-        {
-            free(trim);
-            continue;
-        }
-        if(ft_strchr("01NSEW", trim[0]))
-        {
-            parser->start_line_map = i;
-            free(trim);
-            break;
-        }
+		trim = ft_strtrim(parser->map2d[i], " \t");
+		if (!trim || !*trim)
+			free(trim);
+        
     }
     return (0);
 }

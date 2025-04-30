e6c3745bf5bcac73754e2e9a82f2de211590c49d
tndreka <tndreka@student.42heilbronn.de>
2025-04-30 19:40:18 +0200

    REMOVE: remove the simmple 2d map

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 21:37:11 by tndreka           #+#    #+#             */
-/*   Updated: 2025/03/20 18:03:28 by silndoj          ###   ########.fr       */
+/*   Updated: 2025/04/30 16:43:45 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,76 +34,12 @@ int run_game(t_game *game)
     memset(img->pixels, 255, img->width * img->height * BPP);
 
     mlx_image_to_window(game->mlx, img, 0, 0);
-    //draw map
-    simple_map(img);
     mlx_loop(game->mlx);
     mlx_terminate(game->mlx);
 
     return (EXIT_SUCCESS);
 }
 
-void simple_map(mlx_image_t* img)
-{
-    char **map;
-
-    map = ft_malloc(sizeof(char *) * 11);
-    map[0] = ft_strdup("1111111111");
-    map[1] = ft_strdup("1000000001");
-    map[2] = ft_strdup("1000000001");
-    map[3] = ft_strdup("1000000001");
-    map[4] = ft_strdup("1000101001");
-    map[5] = ft_strdup("1000000001");
-    map[6] = ft_strdup("1000000001");
-    map[7] = ft_strdup("1000000001");
-    map[8] = ft_strdup("1000000001");
-    map[9] = ft_strdup("1111111111");
-    map[10] = 0;
-
-    int x;
-    int y;
-    int sq_size_x;
-    int sq_size_y;
-
-    y = 0;
-    sq_size_x = 800  / 10; // width of the window divided by number of colums
-    sq_size_y =  800 / 10;  // window height  devided by  number of rows
-    while (map[y])
-    {
-        x = 0;
-        while(map[y][x])
-        {
-            for (int i =0; i < sq_size_x; i++)
-                for(int j = 0; j <sq_size_y; j++)
-                {
-                    if(map[y][x] == '1')
-                        mlx_put_pixel(img, x * sq_size_x + i, y * sq_size_y + j, 0x000000FF);
-                    else
-                    {
-                        mlx_put_pixel(img, x * sq_size_x + i, y * sq_size_y + j, 0xFF0000FF);
-                        for( int k = 0; k < sq_size_x; k++)
-                        {
-                            mlx_put_pixel(img, x * sq_size_x + k, y *  sq_size_y,  0xFFFFFFFF);
-                        }
-                        for(int k = 0; k < sq_size_x; k++)
-                        {
-                            mlx_put_pixel(img, x * sq_size_x + k, y *  sq_size_y + sq_size_x - 1,  0xFFFFFFFF);
-                        }
-                        for( int k = 0; k < sq_size_x; k++)
-                        {
-                            mlx_put_pixel(img, x * sq_size_x, y *  sq_size_y + k,  0xFFFFFFFF);
-                        }
-                        for( int k = 0; k < sq_size_x; k++)
-                        {
-                            mlx_put_pixel(img, x * sq_size_x  + sq_size_y  - 1, y *  sq_size_y + k,  0xFFFFFFFF);
-                        }
-                    }
-
-                }
-            x++;
-        }
-        y++;
-    }
-}
 
 int main(int ac, char *av[])
 {

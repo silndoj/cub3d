b162307301f8f3fc697007313a707c302be8ae25
Silvestri Ndoj <silvesterndoj13@gmail.com>
2025-05-03 22:10:16 +0200

    ADD: a way to clear the backtrack of the square while moving was to reset the screen pixels to NULL after every change of position (before the player press the key)

--- src/main.c
+++ src/main.c
@@ -6,11 +6,30 @@
 /*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 21:37:11 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/03 21:07:08 by silndoj          ###   ########.fr       */
+/*   Updated: 2025/05/03 22:08:30 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/cub3d.h"
+
+void clear_image(mlx_image_t *img)
+{
+	uint32_t x;
+	uint32_t y;
+
+	y = 0;
+	x = 0;
+	while (y < img->height)
+	{
+		x = 0;
+		while (x < img->width)
+		{
+			mlx_put_pixel(img, x, y, 0x00000000);
+			x++;
+		}
+		y++;
+	}
+}
 
 void	draw_square(int x, int y, int size, int color, mlx_image_t *img)
 {
@@ -34,6 +53,7 @@ void	ft_hook(void *param)
 {
 	t_game *game;
 	game = param;
+	clear_image(game->img);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_UP))
 		game->player.py -= 5;
 	if (mlx_is_key_down(game->mlx, MLX_KEY_DOWN))
@@ -55,7 +75,6 @@ int run_game(t_game *game)
 		errno_error_mlx();
 	game->player.py = HEIGHT / 2;
 	game->player.px = WIDTH / 2;
-	draw_square(game->player.px, game->player.py, 10, 0x00FF00FF, game->img);
 	mlx_image_to_window(game->mlx, game->img, 0, 0);
 	mlx_loop_hook(game->mlx, ft_hook, game);
     mlx_loop(game->mlx);

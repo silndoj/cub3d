758b339fe1c7124ed17200176ae5259049dc794b
tndreka <tndreka@student.42heilbronn.de>
2025-05-03 20:49:07 +0200

    ADDED: a check that will detirmind the map position and if it is not going to be only at the end it will stop the program and output and error message

--- src/game_build/handle_textures.c
+++ src/game_build/handle_textures.c
@@ -6,11 +6,18 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:23:58 by tndreka           #+#    #+#             */
-/*   Updated: 2025/04/30 19:25:12 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/03 20:46:47 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
+
+static int check_map_pos(char *trim)
+{
+	if (ft_strchr("01NSWE", trim[0]))
+		return(printf("bad_map POSITIOn\n"), 1);
+	return 0;
+}
 
 int    parse_textures(t_parser *parser)
 {
@@ -24,7 +31,8 @@ int    parse_textures(t_parser *parser)
 		trim = ft_strtrim(parser->map2d[i], " \t");
 		if (!trim || !*trim)
 			free(trim);
-        
+        if(check_map_pos(trim))
+			return (free(trim), 1);
     }
     return (0);
 }

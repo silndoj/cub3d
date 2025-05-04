534333ebc78b678632030c8fc398c236b43eb07d
tndreka <tndreka@student.42heilbronn.de>
2025-05-04 19:41:54 +0200

    ADD: fix some norminette error's

--- src/game_build/init_map.c
+++ src/game_build/init_map.c
@@ -6,18 +6,18 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 18:05:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/03 23:26:17 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/04 19:36:20 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-void    init_textures(t_parser *parser)
+void	init_textures(t_parser *parser)
 {
-    parser->no_texture = NULL;
-    parser->so_texture = NULL;
-    parser->we_texture = NULL;
-    parser->ea_texture = NULL;
+	parser->no_texture = NULL;
+	parser->so_texture = NULL;
+	parser->we_texture = NULL;
+	parser->ea_texture = NULL;
 	parser->map_started = false;
 	parser->all_elements = false;
 	parser->no_found = false;
@@ -27,4 +27,3 @@ void    init_textures(t_parser *parser)
 	parser->f_found = false;
 	parser->c_found = false;
 }
-

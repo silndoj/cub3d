904ce63e974218cd5c05863d7aa0982349ef72ec
tndreka <tndreka@student.42heilbronn.de>
2025-05-04 19:40:51 +0200

    ADD: fix some norminette error's

--- src/main.c
+++ src/main.c
@@ -6,48 +6,40 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 21:37:11 by tndreka           #+#    #+#             */
-/*   Updated: 2025/04/30 16:43:45 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/04 19:39:41 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/cub3d.h"
-
 #define BPP sizeof(int32_t)
 
-int run_game(t_game *game)
+int	run_game(t_game *game)
 {
-//    mlx_set_setting(MLX_MAXIMIZED, true);
-    game->mlx = mlx_init(800, 800, "cub3d", true);
-    if (!game->mlx)
-    {
-        fprintf(stderr, "Error: mlx_init failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    mlx_image_t* img = mlx_new_image(game->mlx, 800, 800); // Proper width and height
-    if (!img)
-    {
-        fprintf(stderr, "Error: mlx_new_image failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(img->pixels, 255, img->width * img->height * BPP);
-
-    mlx_image_to_window(game->mlx, img, 0, 0);
-    mlx_loop(game->mlx);
-    mlx_terminate(game->mlx);
-
-    return (EXIT_SUCCESS);
+	game->mlx = mlx_init(800, 800, "cub3d", true);
+	if (!game->mlx)
+	{
+		fprintf(stderr, "Error: mlx_init failed\n");
+		exit(EXIT_FAILURE);
+	}
+	mlx_image_t* img = mlx_new_image(game->mlx, 800, 800);
+	if (!img)
+	{
+		fprintf(stderr, "Error: mlx_new_image failed\n");
+		exit(EXIT_FAILURE);
+	}
+	memset(img->pixels, 255, img->width * img->height * BPP);
+	mlx_image_to_window(game->mlx, img, 0, 0);
+	mlx_loop(game->mlx);
+	mlx_terminate(game->mlx);
+	return (EXIT_SUCCESS);
 }
 
-
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-    t_game game;
+	t_game	game;
 
-    if (map_build(ac, av, &game.parser))
-        return (free_allocations(),1);
-    run_game(&game);
-    return (free_allocations(),0);
+	if (map_build(ac, av, &game.parser))
+		return (free_allocations(), 1);
+	run_game(&game);
+	return (free_allocations(), 0);
 }
-

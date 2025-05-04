a78f32bd24cb450c23cdaa4b8ab4bf28df9b1e11
tndreka <tndreka@student.42heilbronn.de>
2025-05-04 19:42:54 +0200

    ADD: fix some norminette error's

--- src/inc/cub3d.h
+++ src/inc/cub3d.h
@@ -6,22 +6,9 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 14:16:23 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/03 20:41:09 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/04 19:39:17 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/17 21:37:21 by tndreka           #+#    #+#             */
-/*   Updated: 2025/04/30 02:38:18 by silndoj          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 
 #ifndef CUB3D_H
 # define CUB3D_H
@@ -33,40 +20,44 @@
 # include <math.h>
 # include <stdbool.h>
 # include <errno.h>
-
-
 # include "../../MLX42/include/MLX42/MLX42.h"
 # include "libft/includes/libft.h"
-
-#define WIDTH 1024
-#define HEIGHT 1024
-
+# define WIDTH 1024
+# define HEIGHT 1024
 
 typedef struct s_parser
 {
-    char	**map2d;
-    char    *no_texture;
-    char    *so_texture;
-    char    *we_texture;
-    char    *ea_texture;
-    int     start_line_map;
-}   t_parser;
+	char	**map2d;
+	char	*no_texture;
+	char	*so_texture;
+	char	*we_texture;
+	char	*ea_texture;
+	int		start_line_map;
+	bool	map_started;
+	bool	all_elements;
+	bool	no_found;
+	bool	so_found;
+	bool	we_found;
+	bool	ea_found;
+	bool	f_found;
+	bool	c_found;
+}	t_parser;
 
 typedef struct s_game
 {
-	mlx_t*		mlx;
+	mlx_t		*mlx;
 	t_parser	parser;
-}   t_game;
+}	t_game;
 
 //MAP_BUILD.C
-int		map_build(int argc, char **argv, t_parser *parser);
-int 	check_map(char *file, t_parser *parser);
-void    copy_map(int fd, t_parser *parser);
-void 	print_map(t_parser *parser);
-int    parse_textures(t_parser *parser);
-void 	init_textures(t_parser *parser);
+int			map_build(int argc, char **argv, t_parser *parser);
+int			check_map(char *file, t_parser *parser);
+void		copy_map(int fd, t_parser *parser);
+void		print_map(t_parser *parser);
+int			parse_textures(t_parser *parser);
+void		init_textures(t_parser *parser);
 
 //Util Box
-int	errno_exit(void);
+int			errno_exit(void);
 
 #endif

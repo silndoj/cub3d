15f241a081d45be840c0f65153d3e6bd46a72d61
tndreka <tndreka@student.42heilbronn.de>
2025-05-06 19:34:00 +0200

    ADD: added the check if the path is empty and returns error followed by a new line

--- src/game_build/textures.c
+++ src/game_build/textures.c
@@ -29,25 +29,34 @@ int handle_north_texture(char *trim, t_parser *parser)
 int handle_south_texture(char *trim, t_parser *parser)
 {
 	parser->so_found = true;
-	parser->so_texture = ft_strtrim(trim + 3, " \t");
-	if (!parser->so_texture)
+	parser->so_texture = ft_strtrim(trim + 3, " \t\n");
+	if (!parser->so_texture || parser->so_texture[0] == '\0')
+	{
+		free(parser->so_texture);
 		exit_error("Failed to find the path for SO_texture");
+	}
 	return 0;
 }
 int handle_east_texture(char *trim, t_parser *parser)
 {
 	parser->ea_found = true;
-	parser->ea_texture = ft_strtrim(trim + 3, " \t");
-	if (!parser->ea_texture)
+	parser->ea_texture = ft_strtrim(trim + 3, " \t\n");
+	if (!parser->ea_texture || parser->ea_texture[0] == '\0')
+	{
+		free(parser->ea_texture);
 		exit_error("Failed to find the path for EA_texture");
+	}
 	return 0;
 }
 
 int handle_west_texture(char *trim, t_parser *parser)
 {
 	parser->we_found = true;
-	parser->we_texture = ft_strtrim(trim + 3, " \t");
-	if (!parser->we_texture)
+	parser->we_texture = ft_strtrim(trim + 3, " \t\n");
+	if (!parser->we_texture || parser->we_texture[0] == '\0')
+	{
+		free(parser->we_texture);
 		exit_error("Failed to find the path for WE_texture");
+	}
 	return 0;
 }

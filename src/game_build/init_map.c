c3c5e7df8b04ae1dd4eed8d2cb42a3ec2df86f54
tndreka <tndreka@student.42heilbronn.de>
2025-05-06 17:53:17 +0200

    ADD: Edit the name_function

--- src/game_build/init_map.c
+++ src/game_build/init_map.c
@@ -6,13 +6,13 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 18:05:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/04 19:36:20 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/06 16:28:07 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-void	init_textures(t_parser *parser)
+void	init_map(t_parser *parser)
 {
 	parser->no_texture = NULL;
 	parser->so_texture = NULL;

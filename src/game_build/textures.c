57112139c4f337240dc763ac19d24d2772ceb64a
tndreka <tndreka@student.42heilbronn.de>
2025-05-06 18:31:02 +0200

    ADD: made the specific handle_textures function in the same way for NO

--- src/game_build/textures.c
+++ src/game_build/textures.c
@@ -21,3 +21,29 @@ int handle_north_texture(char *trim, t_parser *parser)
 		exit_error("Failed to find the path for NO_texture");
 	return 0;
 }
+
+int handle_south_texture(char *trim, t_parser *parser)
+{
+	parser->so_found = true;
+	parser->so_texture = ft_strtrim(trim + 3, " \t");
+	if (!parser->so_texture)
+		exit_error("Failed to find the path for SO_texture");
+	return 0;
+}
+int handle_east_texture(char *trim, t_parser *parser)
+{
+	parser->ea_found = true;
+	parser->ea_texture = ft_strtrim(trim + 3, " \t");
+	if (!parser->ea_texture)
+		exit_error("Failed to find the path for EA_texture");
+	return 0;
+}
+
+int handle_west_texture(char *trim, t_parser *parser)
+{
+	parser->we_found = true;
+	parser->we_texture = ft_strtrim(trim + 3, " \t");
+	if (!parser->we_texture)
+		exit_error("Failed to find the path for WE_texture");
+	return 0;
+}

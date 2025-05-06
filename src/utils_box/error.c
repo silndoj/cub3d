23ebecda338d6bd23220a8d64a8991a98f5abbdb
tndreka <tndreka@student.42heilbronn.de>
2025-05-06 17:46:00 +0200

    ADD: implemented a function that exits the program and displays an error followed by a new line

--- src/utils_box/error.c
+++ src/utils_box/error.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:02:44 by silndoj           #+#    #+#             */
-/*   Updated: 2025/04/30 02:38:41 by silndoj          ###   ########.fr       */
+/*   Updated: 2025/05/06 16:42:48 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"../inc/cub3d.h"
+
+int	exit_error(const char *msg)
+{
+	printf("Error: %s\n", msg);
+	exit(EXIT_FAILURE);
+}
 
 int	errno_exit(void)
 {

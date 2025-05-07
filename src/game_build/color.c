b2b0d690ac89337a335da0dad27ae65a1d938858
tndreka <tndreka@student.42heilbronn.de>
2025-05-07 18:55:53 +0200

    ADD: the values are outputed correctly

--- src/game_build/color.c
+++ src/game_build/color.c
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 18:13:23 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/07 18:52:46 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/07 18:54:55 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ int	handle_floor_color(char *trim,t_parser  *parser)
 	if (i != 3)
 		exit_error("Invalid Color Format.. try (R,G,B)");
 	parser->floor.r = ft_atoi(value[0]);
+	parser->floor.g = ft_atoi(value[1]);
+	parser->floor.b = ft_atoi(value[2]);
 	printf("%d\n", parser->floor.r);
+	printf("%d\n", parser->floor.g);
+	printf("%d\n", parser->floor.b);
 	return 0;
 }

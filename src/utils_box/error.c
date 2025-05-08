5e5f96505e1f3bd761e919ba0d01c4c2485c9ece
tndreka <tndreka@student.42heilbronn.de>
2025-05-08 22:48:25 +0200

    s

--- src/utils_box/error.c
+++ src/utils_box/error.c
@@ -6,11 +6,11 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:02:44 by silndoj           #+#    #+#             */
-/*   Updated: 2025/05/07 14:31:28 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/08 21:52:04 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"../inc/cub3d.h"
+#include "../inc/cub3d.h"
 
 int	exit_error(const char *msg)
 {

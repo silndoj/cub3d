a3644de96c1942037ebfba0e6cd73500d1af91c7
tndreka <tndreka@student.42heilbronn.de>
2025-05-08 22:42:08 +0200

    ADD: cleanup function

--- src/game_build/color.c
+++ src/game_build/color.c
@@ -6,63 +6,80 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 18:13:23 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/08 12:43:04 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/08 21:48:00 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-int	handle_floor_color(char *trim,t_parser  *parser)
+static	void	ft_clean_split(char **split, int index)
+{
+	int		j;
+
+	j = 0;
+	while (j < index)
+	{
+		free(split[j]);
+		j++;
+	}
+}
+
+int	handle_floor_color(char *trim, t_parser *parser)
 {
 	char	*floor;
 	char	**value;
+	int		i;
 
 	floor = ft_strtrim(trim + 1, " \t");
 	if (!floor)
 		exit_error("Faild to find FLOOR_COLOR");
 	value = ft_split(floor, ',');
 	free(floor);
-	parser->f_found = true;
-	int i = 0;
+	i = 0;
 	while (value[i])
 		i++;
-	// printf("%d\n", i);
 	if (i != 3)
 		exit_error("Invalid Color Format.. try (R,G,B)");
 	parser->floor.r = ft_atoi(value[0]);
 	parser->floor.g = ft_atoi(value[1]);
 	parser->floor.b = ft_atoi(value[2]);
 	if ((parser->floor.r < 0 || parser->floor.r > 255)
-		||(parser->floor.g < 0|| parser->floor.g > 255)
-		|| (parser->floor.b < 0|| parser->floor.b > 255))
+		|| (parser->floor.g < 0 || parser->floor.g > 255)
+		|| (parser->floor.b < 0 || parser->floor.b > 255))
 		exit_error("R,G,B values should be between the range of 0 to 255");
-	return 0;
+	parser->floor_set = true;
+	parser->f_found = true;
+	ft_clean_split(value, i);
+	free(value);
+	return (0);
 }
 
-
-int	handle_ceiling_color(char *trim,t_parser  *parser)
+int	handle_ceiling_color(char *trim, t_parser *parser)
 {
 	char	*ceiling;
 	char	**value;
+	int		i;
 
 	ceiling = ft_strtrim(trim + 1, " \t");
 	if (!ceiling)
 		exit_error("Faild to find CEILING_COLOR");
 	value = ft_split(ceiling, ',');
 	free(ceiling);
-	parser->c_found = true;
-	int i = 0;
+	i = 0;
 	while (value[i])
 		i++;
-	// printf("%d\n", i);
 	if (i != 3)
 		exit_error("Invalid Color Format.. try (R,G,B)");
 	parser->ceiling.r = ft_atoi(value[0]);
 	parser->ceiling.g = ft_atoi(value[1]);
 	parser->ceiling.b = ft_atoi(value[2]);
 	if ((parser->ceiling.r < 0 || parser->ceiling.r > 255)
-		||(parser->ceiling.g < 0|| parser->ceiling.g > 255)
-		|| (parser->ceiling.b < 0|| parser->ceiling.b > 255))
+		|| (parser->ceiling.g < 0 || parser->ceiling.g > 255)
+		|| (parser->ceiling.b < 0 || parser->ceiling.b > 255))
 		exit_error("R,G,B values should be between the range of 0 to 255");
-	return 0;
+	parser->ceiling_set = true;
+	parser->c_found = true;
+	ft_clean_split(value, i);
+	free(value);
+	return (0);
 }

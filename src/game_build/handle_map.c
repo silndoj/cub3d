a07eb8fdc40670e530169145700383cb1ee86333
tndreka <tndreka@student.42heilbronn.de>
2025-05-10 01:00:02 +0200

    ADD: edited the map_line() to proper validate the elemnts in the map. Since i have updated the copy_map() i also updated the find_start_of_map(). check_allelements() added more checks for spaces and tabs. updated the fill of the mapto handle tabs and spaces and cheks for player and aadded the cordinates of the player and the player direction. udated the check_walls() with proper walidations for not closing walls in each side of the map

--- src/game_build/handle_map.c
+++ src/game_build/handle_map.c
@@ -12,12 +12,29 @@
 
 #include "../inc/cub3d.h"
 
+static int ft_is_emty_line(char *line)
+{
+	while (*line)
+	{
+		if(!ft_isspace((int)*line))
+			return (0);
+		line++;
+	}
+	
+	return (1);
+}
+
 static	int	map_line(char *trim)
 {
+	int has_valid_char;
+
+	has_valid_char = 0;
 	while (*trim)
 	{
 		if (!ft_strchr(" 01NSWE", *trim))
 			return (0);
+		if (ft_strchr("01NSWE", *trim))
+			has_valid_char = 1;
 		trim++;
 	}
 	return (1);
@@ -25,28 +42,24 @@ static	int	map_line(char *trim)
 
 static	int	find_start_of_map(t_parser *parser, int start_i)
 {
-	char	*trim;
+	// char	*trim;
 	int		i;
 
 	i = start_i - 1;
 	while (parser->map2d[++i])
 	{
-		trim = ft_strtrim(parser->map2d[i], " \t\n");
-		if (trim && *trim)
+		// trim = ft_
+		strtrim(parser->map2d[i], " \t\n");
+		if (!parser->map2d[i] || !*parser->map2d[i])
+			ft_is_emty_line(parser->map2d[i]);
+			continue;
+		if (map_line(parser->map2d[i]))
 		{
-			if (map_line(trim))
-			{
-				parser->start_line_map = i;
-				free(trim);
-				return (0);
-			}
-			// else
-			// {
-				free(trim);
-				exit_error("Invalid line of map");
-			// }
+			parser->start_line_map = i;
+			return (0);
 		}
-		free(trim);
+		else
+		 	continue;
 	}
 	exit_error("Could not find start of map");
 	return (1);
@@ -54,17 +67,17 @@ static	int	find_start_of_map(t_parser *parser, int start_i)
 
 static	int	check_all_elements_file(char *trim, t_parser *parser)
 {
-	if (!ft_strncmp(trim, "NO ", 3))
+	if (!ft_strncmp(trim, "NO ", 3) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_north_texture(trim, parser);
-	else if (!ft_strncmp(trim, "SO ", 3))
+	else if (!ft_strncmp(trim, "SO ", 3) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_south_texture(trim, parser);
-	else if (!ft_strncmp(trim, "EA ", 3))
+	else if (!ft_strncmp(trim, "EA ", 3) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_east_texture(trim, parser);
-	else if (!ft_strncmp(trim, "WE ", 3))
+	else if (!ft_strncmp(trim, "WE ", 3) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_west_texture(trim, parser);
-	else if (!ft_strncmp(trim, "F ", 2))
+	else if (!ft_strncmp(trim, "F ", 2) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_floor_color(trim, parser);
-	else if (!ft_strncmp(trim, "C ", 2))
+	else if (!ft_strncmp(trim, "C ", 2) && (trim[2] != ' ' || trim[2] == '\t'))
 		handle_ceiling_color(trim, parser);
 	if (parser->no_found && parser->so_found && parser->ea_found
 		&& parser->we_found && parser->f_found && parser->c_found)
@@ -75,74 +88,80 @@ static	int	check_all_elements_file(char *trim, t_parser *parser)
 static	int	create_int_array(t_parser *parser)
 {
 	int		i;
+	int		j;
 
-	parser->map = ft_malloc(sizeof(int *) * parser->map_height);
-	if (!parser->map)
+
+	parser->map1 = ft_malloc(sizeof(int *) * parser->map_height);
+	if (!parser->map1)
 		exit_error("Failed to alocate memory for rows");
 	i = 0;
 	while (i < parser->map_height)
 	{
-		parser->map[i] = ft_malloc(sizeof(int) * parser->map_width);
-		if (!parser->map[i])
+		parser->map1[i] = ft_malloc(sizeof(int) * parser->map_width);
+		if (!parser->map1[i])
 			exit_error("Failed to alocate memory for col");
-
+		j = 0;
+		while (j < parser->map_width)
+		{
+			parser->map1[i][j] = -1;
+			j++;
+		}
 		i++;
 	}
 	return (0);
 }
 
-static	void fill_map(t_parser *parser, char c, int len)
+static	int fill_map(t_parser *parser, char c, int len)
 {
-	if (parser->x < len)
-	{
-		c = parser->map2d[parser->start_line_map][parser->x];
-		if (c == '\t')
-			c = ' ';
-	}
-	else
-		c = ' ';
-	if (c == '1')
-		parser->map[parser->y][parser->x] = 1;
-	else if (c == '0')
-		parser->map[parser->y][parser->x] = 0;
-	else if (c == ' ')
-		parser->map[parser->y][parser->x] = -1;
-	else if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
-	{
-		if (parser->player_found)
-			exit_error("Multiple players found");
-		if (parser->y == 0 || parser->y == parser->map_height - 1
-			|| parser->x == 0 || parser->x == parser->map_width - 1)
-			exit_error("Player on edge");
-		parser->map[parser->y][parser->x] = 8;
-		parser->player_found = true;
-	}
-	else
-		exit_error("Invalid character");
-}
+	int start_line;
+	int x;
+	int y;
+	char c;
 
-static	int	put_map_elements(t_parser *parser)
-{
-	int		len;
-	char	c;
-
-	c = '\0';
-	parser->y = 0;
-	len = 0;
-	while (parser->y < parser->map_height && parser->map2d[parser->start_line_map])
+	start_line = parser->start_line_map;
+	y = 0;
+	x = 0;
+	parser->player_found = false;
+	while (y < parser->map_height && parser->map2d[parser->start_line_map])
 	{
-		len = ft_strlen(parser->map2d[parser->start_line_map]);
-		parser->x = 0;
-		while (parser->x < parser->map_width)
+		len = ft_strlen(parser->map2d[parse_maper->start_line_map]);
+		x = 0;
+		while (x < parser->map_width)
 		{
-			fill_map(parser, c, len);
-			parser->x++;
+			if (x < len)
+			{
+				c = parser.map2d[parser->start_line_map][x];
+			}
+			else
+				c = ' ';
+			if (c == '1')
+				parser->map1[y][x] = 1;
+			else if (c == '0')
+				parser->map1[y][x] = 0;
+			else if (c == ' ' || c == '\t')
+				parser->map1[y][x] = -2;
+			else if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
+			{
+				if (parser->player_found)
+					exit_error("Miltiple players found");
+			}
+			parser->map1[y][x] = 0;
+			parser->player_found = true;
+			parser->player_pos_x = x;
+			parser->player_pos_y = y;
+			parse_mapr->player_direction = c;
+			else if (c != '\n' && c == != '\0')
+				exit_error("Invalid character");
+			x++;
 		}
-		parser->y++;
-		parser->start_line_map++;
+		y++;
+		parser->start_line_map++;		
 	}
+	parser->y2 = y;
+	parser->x = x;
 	if (!parser->player_found)
 		exit_error("Missing player position");
+	parser->start_line_map = start_line;
 	return (0);
 }
 
@@ -152,27 +171,42 @@ static	void convert_map_to_int(t_parser *parser)
 	int		map_width;
 	int		map_height;
 	int		len;
-	char	*trim;
 
 	i = parser->start_line_map;
 	map_width = 0;
 	map_height = 0;
 	len = 0;
+
 	while (parser->map2d[i])
 	{
-		trim = ft_strtrim(parser->map2d[i], " \t\n");
-		if (trim && *trim)
+	
+		if (map_line(parser->map2d[i]))
 		{
 			map_height++;
-			len = ft_strlen(trim);
+			len = ft_strlen(parser->map2d[i]);
 			if (len > map_width)
 				map_width = len;
 		}
-		free(trim);
+		else if (!ft_is_emty_line(parser->map2d[i]))
+			break;
+		else if (map_height > 0)
+		{
+			map_height++;
+			len = ft_strlen(parser->map2d[i]);
+			if (len > map_width)
+				map_width = len;
+		}
+		if (trim && *trim)
+		{
+			map_height++;
+		}
 		i++;
 	}
 	parser->map_height = map_height;
 	parser->map_width = map_width;
+	if (map_height == 0 || map_width == 0)
+		exit_error("Map is empty");
+	return (0);
 }
 
 static void check_all(t_parser *parser)
@@ -213,98 +247,80 @@ static void extra_char(t_parser *parser)
 	}
 }
 
-// static void map_pos(t_parser *parser, int map_start)
-// {
-// 	int		i;
-// 	char	*trim;
-
-// 	i = 0;
-// 	while (i < map_start)
-// 	{
-// 		trim = ft_strtrim(parser->map2d[i], " \t\n");
-// 		if (trim && *trim)
-// 		{
-// 			free(trim);
-// 			exit_error("Map should appear in the end of the file");
-// 		}
-// 		free(trim);
-// 		i++;
-// 	}
-// }
-
-static void clean_up(t_parser *parser, int **wall)
+static int flood_fill(t_parser *parser, int **wall, int y, int x)
 {
-	int i;
-
-	i = 0;
-	while (i < parser->map_height)
-	{
-		free(wall[i]);
-		i++;
-	}
-}
-
-static void flood_fill(t_parser *parser, int **wall, int y, int x)
-{
-	if (x < 0 || y < 0 || y >= parser->map_height || x >= parser->map_width)
-		exit_error("Map is not closed by walls");
-	if (parser->map[y][x] == -1)
-		exit_error("Map is not closed by walls");
-	if (wall[y][x] || parser->map[y][x] == 1)
-		return ;
+	if (y < 0 || y >= parser->map_height || x < 0 || x >= parser->map_width)
+		return (0);
+	if (wall[y][x] == 1 || parser->map[y][x] == 1)
+		return (0);
 	wall[y][x] = 1;
+	if (y == 0 || y == parser->map_height - 1
+		|| x == 0 || x == parser->map_width - 1)
+	{
+		parser->invalid_map = true;
+		exit_error("Map is not closed by walls");
+	}
 	flood_fill(parser, wall, y + 1, x);
 	flood_fill(parser, wall, y - 1, x);
 	flood_fill(parser, wall, y, x + 1);
 	flood_fill(parser, wall, y, x - 1);
+
+
+	flood_fill(parser, wall, y + 1, x + 1);
+	flood_fill(parser, wall, y + 1, x - 1);
+	flood_fill(parser, wall, y - 1, x + 1);
+	flood_fill(parser, wall, y - 1, x - 1);
+	return (0);
 }
+
 
 static void check_walls(t_parser *parser)
 {
-	//alloc memory
 	int **wall;
 	int	i;
 
+	parser->invalid_map = false;
 	wall = ft_malloc(sizeof(int *) * parser->map_height);
-	if(!wall)
 		exit_error("faild alocation for check_walls()->H");
+	if(!wall)
+	i = 0;
+	while (i++ < parser->map_height)
+		wall[i] = NULL;
 	i = 0;
 	while (i < parser->map_height)
 	{
 		wall[i] = ft_calloc(parser->map_width, sizeof(int));
 		if(!wall[i])
-		{
-			clean_up(parser, wall);
-			free(wall);
 			exit_error("failed alocation in check_wall()->W");
-		}
 		i++;
 	}
-	//check
-	int y;
-	int x;
-	int start;
-
-	y =0;
-	start = 0;
-
-	while (y < parser->map_height)
+	if (parser->player_found)
 	{
-		x = 0;
-		while (x < parser->map_width)
+		flood_fill(parser, wall, parser->player_pos_y, parser->player_pos_x);
+		if (parser->invalid_map)
+			exit_error("Map is not closed by walls");
+	}
+	else
+	{
+		int y = 0;
+		while (y < parser->map_height)
 		{
-			if ((parser->map[y][x] == 0 || parser->map[y][x] == 8 ) )
+			int x = 0;
+			while (x < parser->map_width)
 			{
-				flood_fill(parser, wall, y, x);
-				start = 1;
+				if (parser->map[y][x] == 0 )
+				{
+					flood_fill(parser, wall, y, x);
+					if(parser->invalid_map)
+						exit_error("Map is not closed by walls");
+					y = parser->map_height;
+					break;
+				}
 			}
 			x++;
 		}
 		y++;
 	}
-	//clean up 
-	clean_up(parser, wall);
-	free(wall);
 }
 
 int	parse_map(t_parser *parser)
@@ -317,21 +333,17 @@ int	parse_map(t_parser *parser)
 	{
 		trim = ft_strtrim(parser->map2d[i], " \t\r\n");
 		if (trim && *trim)
-		{
-			if(check_all_elements_file(trim, parser))
-				return (free(trim), 1);
-		}
+			check_all_elements_file(trim, parser);
 		free(trim);
 	}
-	if (!parser->no_found || !parser->so_found || !parser->ea_found
-		|| !parser->we_found || !parser->f_found || !parser->c_found)
-	{
-		check_all(parser);
-	}
+	// if (!parser->no_found || !parser->so_found || !parser->ea_found
+	// 	|| !parser->we_found || !parser->f_found || !parser->c_found)
+	// {
+		// }
+	check_all(parser);
 	find_start_of_map(parser, i + 1);
 	convert_map_to_int(parser);
-	if (create_int_array(parser))
-		return (1);
+	create_int_array(parser);
 	put_map_elements(parser);
 	check_walls(parser);
 	extra_char(parser);

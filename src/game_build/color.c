2c6967e2b3888ded62936157b8dcd2146a47b579
tndreka <tndreka@student.42heilbronn.de>
2025-05-09 19:46:11 +0200

    ADD: added checks for values of colors and in case of error i free and display error

--- src/game_build/color.c
+++ src/game_build/color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 18:13:23 by tndreka           #+#    #+#             */
-/*   Updated: 2025/05/08 21:48:00 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/05/09 19:44:35 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,26 @@ int	handle_floor_color(char *trim, t_parser *parser)
 	i = 0;
 	while (value[i])
 		i++;
-	if (i != 3)
+	if (!value || i != 3)
+	{
+		if(value)
+		{
+			ft_clean_split(value, i);
+			free(value);
+		}
 		exit_error("Invalid Color Format.. try (R,G,B)");
+	}
 	parser->floor.r = ft_atoi(value[0]);
 	parser->floor.g = ft_atoi(value[1]);
 	parser->floor.b = ft_atoi(value[2]);
 	if ((parser->floor.r < 0 || parser->floor.r > 255)
 		|| (parser->floor.g < 0 || parser->floor.g > 255)
 		|| (parser->floor.b < 0 || parser->floor.b > 255))
+	{
+		ft_clean_split(value, i);
+		free(value);
 		exit_error("R,G,B values should be between the range of 0 to 255");
+	}
 	parser->floor_set = true;
 	parser->f_found = true;
 	ft_clean_split(value, i);
@@ -68,15 +79,26 @@ int	handle_ceiling_color(char *trim, t_parser *parser)
 	i = 0;
 	while (value[i])
 		i++;
-	if (i != 3)
+	if (!value || i != 3)
+	{
+		if(value)
+		{
+			ft_clean_split(value, i);
+			free(value);
+		}
 		exit_error("Invalid Color Format.. try (R,G,B)");
+	}
 	parser->ceiling.r = ft_atoi(value[0]);
 	parser->ceiling.g = ft_atoi(value[1]);
 	parser->ceiling.b = ft_atoi(value[2]);
 	if ((parser->ceiling.r < 0 || parser->ceiling.r > 255)
 		|| (parser->ceiling.g < 0 || parser->ceiling.g > 255)
 		|| (parser->ceiling.b < 0 || parser->ceiling.b > 255))
+	{
+		ft_clean_split(value, i);
+		free(value);
 		exit_error("R,G,B values should be between the range of 0 to 255");
+	}
 	parser->ceiling_set = true;
 	parser->c_found = true;
 	ft_clean_split(value, i);
